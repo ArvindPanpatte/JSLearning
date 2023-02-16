b4378c10d17390158d62673de18e098fed1e065a
ArvindPanpatte <arvindpanpatte99@gmail.com>
2023-02-16 19:05:03 +0530

    variableAssign Program is created
    variableAssign Program is created

---
+++ 01_variableAssign.js
@@ -0,0 +1,19 @@
+console.log("\t\t ----------Step 1.----------");
+var myFullName;
+console.log(myFullName);
+myFullName="Arvind Panpatte";
+console.log("My Name:",myFullName);
+myFullName="Arvind Kishanrao Panpatte";
+console.log("My Full Name:",myFullName);
+console.log("_______________________________________________________");
+console.log("\t\t ----------Step 2.----------");
+var myLove="I love only JS";
+console.log(myLove);
+console.log("_______________________________________________________");
+console.log("\t\t ----------Step 3.----------");
+var firstName="Arvind",lastName="Panpatte",age="31",address="Nanded",pincode="431605",gender="Male",isMarried="YES",midName="Kishanrao";
+// adress="Nanded",pincode="431605",gender="Male",isMarried="YES";
+// console.log("First Name:",firstName,"\nLast Name:",lastName,"\nAge:",age,"\nAddress:",Nanded,"\nPincode:",pincode,"\nGender:",gender,"\nis Married:",isMarried);
+console.log("First Name:\t",firstName,"\nMiddle Name:\t",midName,"\nLast Name:\t",lastName,"\nAge:\t\t",age,"\nAddress:\t",address,"\nPincode:\t",pincode,"\nGender:\t\t",gender,"\nisMarried:\t",isMarried);
+console.log("_______________________________________________________");
+
